c5dec58cb04978df9e027140ebcc99ae2168bff2
mysmolthing <mysmolthing@gmail.com>
2023-10-01 17:26:52 +0800

    Add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,11 +19,11 @@ module.exports = {
           path: "/blog", // Defines the slug for the blog listing page
           usePathPrefixForArticles: false, // Default true (i.e. path will be /blog/first-article)
         },
-        // googleAnalytics: {
-        //     trackingId: "UA-XXXXXX-X",
-        //     anonymize: true, // Default true
-        //     environments: ["production", "development"] // Default ["production"]
-        // }
+        googleAnalytics: {
+          trackingId: "G-1JZLF5MWEN",
+          //     anonymize: true, // Default true
+          environments: ["production"], // Default ["production"]
+        },
       },
     },
   ],
